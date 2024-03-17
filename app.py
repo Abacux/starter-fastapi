c8a6ea88e85e5880fb68c180370915105024bb20
Abacux <159709109+Abacux@users.noreply.github.com>
2024-03-17 17:06:09 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,19 +5,6 @@ import pymongo
 from typing import Union, List
 from pydantic import BaseModel
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 #-------------usersDataBase------------#
