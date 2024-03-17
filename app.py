7cbb9e906e5fc02c99033b875eeff4bd5c10fbf7
Abacux <159709109+Abacux@users.noreply.github.com>
2024-03-17 16:56:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,91 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
-
+#API Functions
+#--------------------------------------
+#Imports
+import pymongo
+from typing import Union, List
 from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://127.0.0.1:5500",
+    "http://localhost:8080",
+]
 
-class Item(BaseModel):
-    item_id: int
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
+#-------------usersDataBase------------#
+
+#Conexion
+client = pymongo.MongoClient('mongodb+srv://issaiase64:abacux64@cluster0.calbbx3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+basedD = client['Gym']
+collection = basedD['usuarios']
+#Esquema
+class User(BaseModel):
+   id: str
+   userName: str
+   password: str
+   name: str
+   lastName: str
+   genere: str
+   inscription: str
+   membership: str
+   remainingDays: str
+
+"""{
+  "_id": "id",
+  "userName": "userName",
+  "password": "password",
+  "name": "name",
+  "lastName": "lastName",
+  "genere": "genere",
+  "inscription": "inscription",
+  "membership": "membership",
+  "remainingDays": "remainingDays"
+}"""
+
+def userSchema(user):
+   return {"_id": str(user["id"]),
+            "userName": user["userName"],
+            "password": user["password"],
+            "name": user["name"],
+            "lastName": user["lastName"],
+            "genere": user["genere"],
+            "inscription": user["inscription"],
+            "membership": user["membership"],
+            "remainingDays": user["remainingDays"]}
+
+#Funciones
+#find
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def read_root():
+   return {"Hello": "World"}
+
+@app.get("/users/")
+async def getUsers():
+      data ={}
+      for u in collection.find({}):
+          data[str(u["_id"])] = u
+      return data
 
 
-@app.get('/favicon.ico', include_in_schema=False)
-async def favicon():
-    return FileResponse('favicon.ico')
 
+@app.post("/new-user/")
+async def postData(modelo:User):
+   dict_modelo = dict(modelo)
+   idValue= dict_modelo["id"]
+   dict_modelo.pop("id")
+   dict_modelo["_id"]=idValue
 
-@app.get("/item/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
-
-
-@app.get("/items/")
-async def list_items():
-    return [{"item_id": 1, "name": "Foo"}, {"item_id": 2, "name": "Bar"}]
-
-
-@app.post("/items/")
-async def create_item(item: Item):
-    return item
+   collection.insert_one(dict_modelo)
