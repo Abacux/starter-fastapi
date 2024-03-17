fdf07268840e3888bb99913dc855b0686380358b
Abacux <159709109+Abacux@users.noreply.github.com>
2024-03-17 16:57:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://127.0.0.1:5500",
-    "http://localhost:8080",
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
